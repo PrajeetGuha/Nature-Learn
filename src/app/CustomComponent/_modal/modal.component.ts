a21d7ada928e5e81803c52c487a87838f87e0a96
PrajeetGuha <prajeetguha@gmail.com>
2021-12-21 23:45:43 +0530

    minor fixes

--- src/app/CustomComponent/_modal/modal.component.ts
+++ src/app/CustomComponent/_modal/modal.component.ts
@@ -17,39 +17,32 @@ export class ModalComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit(): void {
-        // ensure id attribute exists
         if (!this.id) {
             console.error('modal must have an id');
             return;
         }
 
-        // move element to bottom of page (just before </body>) so it can be displayed above everything else
         document.body.appendChild(this.element);
 
-        // close modal on background click
         this.element.addEventListener('click', (el: { target: { className: string; }; }) => {
             if (el.target.className === 'jw-modal') {
                 this.close();
             }
         });
 
-        // add self (this modal instance) to the modal service so it's accessible from controllers
         this.modalService.add(this);
     }
 
-    // remove self from modal service when component is destroyed
     ngOnDestroy(): void {
         this.modalService.remove(this.id);
         this.element.remove();
     }
 
-    // open modal
     open(): void {
         this.element.style.display = 'block';
         document.body.classList.add('jw-modal-open');
     }
 
-    // close modal
     close(): void {
         this.element.style.display = 'none';
         document.body.classList.remove('jw-modal-open');
